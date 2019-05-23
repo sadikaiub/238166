6cf1374288b94f44307dfcbe32166ebd82d1b3d6
shihabrpi15 <shihabrpi15@yahoo.com>
2019-05-23 10:49:38 +0600

    reverse button

--- MyWinApp/MyWinApp/ArrayForm.Designer.cs
+++ MyWinApp/MyWinApp/ArrayForm.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.richTextBox = new System.Windows.Forms.RichTextBox();
             this.ShowButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.ReverseButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBox
@@ -50,16 +53,47 @@
             this.ShowButton.UseVisualStyleBackColor = true;
             this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
             // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(50, 224);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.TabIndex = 2;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(131, 224);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.numberTextBox.TabIndex = 3;
+            // 
+            // ReverseButton
+            // 
+            this.ReverseButton.Location = new System.Drawing.Point(50, 265);
+            this.ReverseButton.Name = "ReverseButton";
+            this.ReverseButton.Size = new System.Drawing.Size(75, 23);
+            this.ReverseButton.TabIndex = 4;
+            this.ReverseButton.Text = "Reverse";
+            this.ReverseButton.UseVisualStyleBackColor = true;
+            this.ReverseButton.Click += new System.EventHandler(this.ReverseButton_Click);
+            // 
             // ArrayForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ReverseButton);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.AddButton);
             this.Controls.Add(this.ShowButton);
             this.Controls.Add(this.richTextBox);
             this.Name = "ArrayForm";
             this.Text = "ArrayForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,5 +101,8 @@
 
         private System.Windows.Forms.RichTextBox richTextBox;
         private System.Windows.Forms.Button ShowButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Button ReverseButton;
     }
 }
--- MyWinApp/MyWinApp/ArrayForm.cs
+++ MyWinApp/MyWinApp/ArrayForm.cs
@@ -18,6 +18,11 @@ namespace MyWinApp
             
         }
 
+        int index = 0;
+        const int size = 10;
+        int[] firstNumber = new int[size];
+
+
         private void ShowButton_Click(object sender, EventArgs e)
         {
             const int size = 10;
@@ -32,9 +37,41 @@ namespace MyWinApp
             for (int index = 0; index < firstNumber.Length; index++)
             {
                 if (firstNumber[index]!=0)
-                message = message +"Value at index: "+ index + " is:"+firstNumber[index]+"\n";
+               message = message +"Value at index: "+ index + " is:"+firstNumber[index]+"\n";
                 richTextBox.Text = message;
             }
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+
+            firstNumber[index] = Convert.ToInt32(numberTextBox.Text);
+            index++;
+            string message = "";
+
+
+            for (int index = 0; index < firstNumber.Length; index++)
+            {
+               if (firstNumber[index] != 0)
+                    message = message + "Value at index: " + index + " is:" + firstNumber[index] + "\n";
+                richTextBox.Text = message;
+            }
+        }
+
+        private void ReverseButton_Click(object sender, EventArgs e)
+        {
+            //firstNumber[index] = Convert.ToInt32(numberTextBox.Text);
+            //index--;
+            string message = "";
+
+
+            for (int index =(firstNumber.Length-1);index>=0 ; index--)
+            {
+                if (firstNumber[index] != 0)
+                    message = message + "Value at index: " + index + " is:" + firstNumber[index] + "\n";
+                richTextBox.Text = message;
+            }
+
         }
     }
 }
